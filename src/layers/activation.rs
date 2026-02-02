7bf808ecb7243e83b7b6cb95d2e31b8ad671388e
Federico Carollo <s299422@studenti.polito.it>
2026-02-02 12:38:56 +0100

    Feat: added structure for layer structs

---
+++ src/layers/activation.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+pub enum Activation {
+    Gelu,
+    Relu,
+}
+
+impl Activation {
+    pub fn from_str(name: &str) -> Self {
+        match name {
+            "gelu" => Activation::Gelu,
+            "relu" => Activation::Relu,
+            _ => panic!("Unsupported activation function: {}", name),
+        }
+    }
+
+    pub fn apply(&self, input: &candle_core::Tensor) -> candle_core::Result<candle_core::Tensor> {
+        match self {
+            Activation::Gelu => input.gelu(),
+            Activation::Relu => input.relu()
+        }
+    }
+}
